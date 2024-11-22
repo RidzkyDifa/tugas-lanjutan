14fdc6b2ce78ec55c839de33769e15ff92edae67
RidzkyDifa <ridzkydifa2@gmail.com>
2024-11-22 22:57:13 +0700

    tugas 2 final

--- src/Component/Con Section/index.jsx
+++ src/Component/Con Section/index.jsx
@@ -2,7 +2,7 @@
 import Header from "../Header";
 const MovieCon = ({ movies, title }) => {
   return (
-    <section className="section-1">
+    <section className="section-0">
       <Header title={title} />
       <div className="con-section">
         {movies.map((movie, index) => (
@@ -11,6 +11,34 @@ const MovieCon = ({ movies, title }) => {
             <div className="con-child">
               <p className="mini-title">{movie.title}</p>
               <p className="mini-rating">{movie.rating} / 5.0</p>
+            </div>
+            <div className="movie-popup0">
+              <img src={movie.hover} alt={movie.title} className="pop-image" />
+              <div className="pop-desc">
+                <div className="icon-pop">
+                  <span className="play-btn">⏵</span>
+                  <span className="checked-btn">✓</span>
+                  <span className="dropdown-btn">˅</span>
+                </div>
+                <div>
+                  <span className="movie-rate">{movie.rate}</span>
+                  <span className="movie-eps">{movie.episodes}</span>
+                </div>
+                <span className="movie-genre">
+                  {movie.genres.map((genre, index) => (
+                    <>
+                      <span key={index} className="genres-item">
+                        {genre}
+                      </span>
+                      <span>
+                        {index < movie.genres.length - 1 && (
+                          <span className="dot">•</span>
+                        )}
+                      </span>
+                    </>
+                  ))}
+                </span>
+              </div>
             </div>
           </div>
         ))}
--- src/Component/Movie Grid/index.jsx
+++ src/Component/Movie Grid/index.jsx
@@ -5,37 +5,39 @@ const Moviegrid = ({ movies }) => {
   return (
     <div className="movie-grid">
       {movies.map((movie, index) => (
-        <div key={index} className="movie-gallery">
-          <img src={movie.src} alt={movie.title} className="movie-poster" />
-          <div className="movie-popup">
-            <img src={movie.hover} alt={movie.title} className="pop-image" />
-            <div className="pop-desc">
-              <div className="icon-pop">
-                <span className="play-btn">⏵</span>
-                <span className="checked-btn">✓</span>
-                <span className="dropdown-btn">˅</span>
+        <>
+          <div key={index} className="movie-gallery">
+            <img src={movie.src} alt={movie.title} className="movie-poster" />
+            <div className="movie-popup">
+              <img src={movie.hover} alt={movie.title} className="pop-image" />
+              <div className="pop-desc">
+                <div className="icon-pop">
+                  <span className="play-btn">⏵</span>
+                  <span className="checked-btn">✓</span>
+                  <span className="dropdown-btn">˅</span>
+                </div>
+                <div>
+                  <span className="movie-rate">{movie.rate}</span>
+                  <span className="movie-eps">{movie.episodes}</span>
+                </div>
+                <span className="movie-genre">
+                  {movie.genres.map((genre, index) => (
+                    <>
+                      <span key={index} className="genres-item">
+                        {genre}
+                      </span>
+                      <span>
+                        {index < movie.genres.length - 1 && (
+                          <span className="dot">•</span>
+                        )}
+                      </span>
+                    </>
+                  ))}
+                </span>
               </div>
-              <div>
-                <span className="movie-rate">{movie.rate}</span>
-                <span className="movie-eps">{movie.episodes}</span>
-              </div>
-              <span className="movie-genre">
-                {movie.genres.map((genre, index) => (
-                  <>
-                    <span key={index} className="genres-item">
-                      {genre}
-                    </span>
-                    <span>
-                      {index < movie.genres.length - 1 && (
-                        <span className="dot">•</span>
-                      )}
-                    </span>
-                  </>
-                ))}
-              </span>
             </div>
           </div>
-        </div>
+        </>
       ))}
     </div>
   );
